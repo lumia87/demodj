fef982e2eb9644b436ed8cae2ff22946874f7bcb
lumia <nguyenbaha@gmail.com>
2022-01-09 09:50:29 +0700

    edited settings.py

--- demodj/settings.py
+++ demodj/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare', 
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'demodj.urls'
